f5bb9b03e6d523d0c608a0cb0ebd2387a88327c1
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-17 23:23:04 +0300

    import routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import routes from './src/routes/nemRaRoutes';
 var express = require('express');
 //import express from 'express;
 
 const app = express();
 const PORT = 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Node and express server started')
