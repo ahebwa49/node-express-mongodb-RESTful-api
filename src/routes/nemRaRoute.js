3b27d30dddb7e13016f4946fe75be4e83208299f
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-17 23:20:21 +0300

    add get endpoint with some middleware

--- src/routes/nemRaRoute.js
+++ src/routes/nemRaRoute.js
@@ -1,5 +1,9 @@
 export const routes = (app) => {
 
   app.route('/contact')
-
+  .get((req, res, next) => {
+    console.log(`request from: ${originalUrl}`)
+    console.log(`type: ${method}`)
+    next();
+  });
 }
