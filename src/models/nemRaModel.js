51154fa287e19616ad8e291ca8105eb289e6863c
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-18 08:58:07 +0300

    add properties to the contact schema

--- src/models/nemRaModel.js
+++ src/models/nemRaModel.js
@@ -3,5 +3,26 @@
   const Schema = mongoose.Schema;
 
   const ContactSchema = new Schema({
-    
-  })
+    firstName:{
+      type: String,
+      required: 'Enter your first name'
+    },
+    lastName:{
+      type: String,
+      required: 'Enter your first name'
+    },
+    email:{
+      type: String
+    },
+    company:{
+      type: String
+    },
+    number:{
+      type: Number
+    },
+    date_created:{
+      type: Date,
+      default: Date.now
+    }
+  });
+  
