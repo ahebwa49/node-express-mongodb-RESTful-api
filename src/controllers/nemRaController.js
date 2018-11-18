b531f7a070f1ed8afa902ca0223943bfa076cfff
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-18 09:18:05 +0300

    define a function, addNewContact

--- src/controllers/nemRaController.js
+++ src/controllers/nemRaController.js
@@ -2,3 +2,7 @@ import mongoose from 'mongoose';
 import {ContactSchema} from '../models/nemRaModel';
 
 const Contact = mongoose.model('Contact', ContactSchema);
+
+export const addNewContact = (req, res) => {
+  
+}
